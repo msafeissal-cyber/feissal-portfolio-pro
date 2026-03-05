3f19a86667b9f079ca9f5f59be7765428327ef4a
msafeissal-cyber <msafeissal@gmail.com>
2026-03-05 17:24:28 +0000

    Update script.js

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -1,1 +1,57 @@
+const emojis = ["🍎","🍌","🍇","🍉","🍎","🍌","🍇","🍉"];
 
+let shuffled = emojis.sort(() => 0.5 - Math.random());
+
+const board = document.getElementById("board");
+
+let firstCard = null;
+let secondCard = null;
+
+shuffled.forEach((emoji) => {
+
+let card = document.createElement("div");
+card.classList.add("card");
+card.dataset.emoji = emoji;
+
+card.addEventListener("click", () => {
+
+if(card.classList.contains("flipped")) return;
+
+card.textContent = emoji;
+card.classList.add("flipped");
+
+if(!firstCard){
+firstCard = card;
+}
+else{
+secondCard = card;
+
+if(firstCard.dataset.emoji === secondCard.dataset.emoji){
+
+firstCard = null;
+secondCard = null;
+
+}else{
+
+setTimeout(()=>{
+
+firstCard.textContent = "";
+secondCard.textContent = "";
+
+firstCard.classList.remove("flipped");
+secondCard.classList.remove("flipped");
+
+firstCard = null;
+secondCard = null;
+
+},1000);
+
+}
+
+}
+
+});
+
+board.appendChild(card);
+
+});
